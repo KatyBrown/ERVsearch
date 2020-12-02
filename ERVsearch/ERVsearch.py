8555a8ba820113025247f1f6ff7f6227559f23de
KatyBrown <thekatybrown@gmail.com>
2020-12-02 15:02:55 +0000

    documentation for region functions

--- ERVsearch/ERVsearch.py
+++ ERVsearch/ERVsearch.py
@@ -1281,12 +1281,44 @@ def makeCleanFastas(infile, outfile):
 
 
 @follows(mkdir("ERV_regions.dir"))
+@follows(makeCleanFastas)
 @merge(makeCleanBeds,
        ["ERV_regions.dir/all_ORFs.bed",
         "ERV_regions.dir/all_regions.bed",
         "ERV_regions.dir/multi_gene_regions.bed",
         "ERV_regions.dir/regions.fasta"])
 def findERVRegions(infiles, outfiles):
+    '''
+    Combines the files containng the ORF regions for the different retroviral
+    genes and merges any regions which are within regions_maxdist of each
+    other to find larger regions containing multiple genes.
+    The `all_ORFs.bed` output file is the concatenated and sorted bed
+    files, `all_regions.bed `contains the merged regions with any ORFs
+    within regions_maxdist of each other (end to end) combined, plus all
+    regions with a single ORF, generated from all_regions.bed using bedtools
+    merge (https://bedtools.readthedocs.io/en/latest/content/tools/merge.html).
+    The name, strand and score columns are concatenated for merged regions,
+    delimited with a ",".
+    `multi_gene_regions.bed` contains only the regions which were found to
+    contain multiple ORFs, `regions.fasta` is the sequence of these region
+    in FASTA format. At this point this includes regions with multiple ORFs
+    from the same gene (e.g. two *pol* ORFs).
+
+    Input Files
+    -----------
+    clean_fastas.dir/*.fasta
+
+    Output Files
+    ------------
+    ERV_regions.dir/all_ORFs.bed
+    ERV_regions.dir/all_regions.bed
+    ERV_regions.dir/multi_gene_regions.bed
+    ERV_regions.dir/regions.fasta
+
+    Parameters
+    ----------
+    [regions] maxdist
+    '''
     combined = Bed.combineBeds(infiles)
     combined.to_csv(outfiles[0], sep="\t", index=None, header=None)
     merged = Bed.mergeBeds(outfiles[0], overlap=PARAMS['regions']['maxdist'],
@@ -1302,9 +1334,70 @@ def findERVRegions(infiles, outfiles):
         "ERV_regions.dir/ERV_regions_final.bed",
         "ERV_regions.dir/ERV_regions_final.fasta"])
 def makeRegionTables(infiles, outfiles):
-    results = Regions.getRegions(infiles[2], genes)
+    '''
+    Takes a merged bed file consisting of regions of the genome identified
+    as having more than one ERV-like ORF, finds the regions within this file
+    which contain more than one different gene (e.g. gag and pol instead of
+    two gag ORFs) and outputs a formatted table of information about these
+    regions.
+
+    The output table (`ERV_regions_final.tsv`) will usually have 37 columns:
+        * name - the final ID of the ERV region - the genes found plus an
+          integer
+          e.g. gag_pol_12
+        * chrom - chromosome
+        * start - start position of the ERV region
+        * end - end position of the ERV region
+        * strand - strand of the ERv region
+        * genus - genus of the ERV region, can be multiple genera delimted by
+          | if different genes had different genera
+        * for each gene screened for (usually gag, pol and env)
+              * GENE_name - the names of the ORFs for this gene in this region
+              * GENE_ID - the original IDs of the ORFs for this gene in this
+                region
+              * GENE_start - the start position of this gene in this region
+                (genome co-ordinates)
+              * GENE_relative_start - the start position of this gene in this
+                region (relative to the start of the region)
+              * GENE_end - the end position of this gene in this region
+                (genome co-ordinates)
+              * GENE_relative_end - the end position of this gene in this
+                region (relative to the start of the region)
+              * GENE_strand - the strand for this gene in this region
+              * GENE_match - the closest reference retrovirus to this gene
+                in this region
+              * GENE_group - the group of the closest reference retrovirus to
+                this gene in this region
+              * GENE_genus - the genus of the closest reference retrovirus to
+                this gene in this region
+        * orig_name - the name of the region in the input table
+    If not all genes are screened for the table will not have the columns
+    for this gene.
+
+    A bed file (ERV_regions_final.bed) is generated with the co-ordinates
+    of the identified regions and a FASTA file (ERV_regions_final.fasta)
+    containing their sequences.
+
+    Input Files
+    -----------
+    ERV_regions.dir/multi_gene_regions.bed
+    grouped.dir/*_groups.tsv
+    genome.fa
+
+    Output Files
+    ------------
+    ERV_regions.dir/ERV_regions_final.tsv
+    ERV_regions.dir/ERV_regions_final.bed
+    ERV_regions.dir/ERV_regions_final.fasta
+
+    Parameters
+    ----------
+    None
+    '''
+    results = Regions.getRegions(infiles[2], genes, log)
     results.to_csv(outfiles[0], sep="\t", index=None)
     cols = HelperFunctions.getBedColumns()
+    # exclude column 5
     cols = cols[:4] + cols[5:]
     results[cols].to_csv(outfiles[1], sep="\t", index=None, header=None)
     Bed.getFasta(outfiles[1], outfiles[2], log)
--- ERVsearch/Regions.py
+++ ERVsearch/Regions.py
@@ -9,10 +9,14 @@ import ORFs
 
 def getDicts(genes, mergecols):
     '''
-    Make a dictionary of dictionaries with a key for each gene.
-    For each column in mergecols geneD[gene][column] is a dictionary where
+    mergecols is the information from the grouped.dir/GENE_groups.tsv which
+    is relevant in the ERV_regions output table - the most similar reference
+    sequence ("match" column), group and genus.
+
+    Returns a dictionary of dictionaries with a key for each gene, for each
+    column in mergecols geneD[gene][column] is a dictionary where
     keys are ERV IDs and values are the value of the column in the
-    grouped.dir/gene_groups.tsv table.
+    grouped.dir/GENE_groups.tsv table.
     '''
     geneD = dict()
     for gene in genes:
@@ -27,29 +31,47 @@ def makeColumns(genes):
     '''
     Get the column names for the various tables
     '''
+    # standard bed file columns
     bed_cols = HelperFunctions.getBedColumns()
+
+    # rename the "name" column as "orig_name" because it will be merged
+    # with a table where "name" exists but is different
     bed_cols[3] = 'orig_name'
-    genes = ['gag', 'pol', 'env']
+
+    # columns from the grouped.dir/GENE_groups.tsv table which have relevant
+    # information
+    # match is the most similar known retrovirus, group is the assigned
+    # retroviral group, genus is the retroviral genus
     merge_cols = ['match', 'group', 'genus']
+
+    # temporary for storing columns to add to keep_cols, final_cols and
+    # all_cols
     new_cols = ['name', 'ID', 'start', 'end', 'strand'] + merge_cols
+
+    # all columns in the final table
     all_cols = ['name']
-    final_cols = []
+
+    # columns to keep in the output table
+
     keep_cols = ['name', 'chrom', 'start', 'end', 'strand', 'genus']
     for gene in genes:
         for col in new_cols:
             all_cols.append("%s_%s" % (gene, col))
-            final_cols.append("%s_%s" % (gene, col))
             keep_cols.append("%s_%s" % (gene, col))
             if col == 'start' or col == "end":
                 keep_cols.append("%s_relative_%s" % (gene, col))
     all_cols.append('orig_name')
     keep_cols.append('orig_name')
-    return (bed_cols, merge_cols, all_cols, final_cols, keep_cols)
+    return (bed_cols, merge_cols, all_cols,  keep_cols)
 
 
 def checkRegion(genes, region):
     '''
-    Check which genes are present in a region.
+    Check which genes are present in a region based on a string containing
+    the concatenated "name" column from the bedtools merged output - the
+    names of the merged regions delimited by ",".
+    e.g. pol247_NW_006711412.1-501724-502357(-),gag154_NW_006711412.1-502959-503325(-)
+    contains pol and gag.
     '''
     region_genes = []
     for gene in genes:
@@ -62,29 +84,41 @@ def processRegion(region, gene, merge_cols, geneD):
     '''
     Take all the instances of a particular gene found in a region and process
     them into columns for the overall regions table.
+    Often there will be more than one ORF associated with a gene, especially
+    the longer genes (usually pol) - this function merges these into a
+    single output.
     Generates a list of 9 results:
-        new_name: the name (ID_chrom_start-end(strand)) of the gene region
+        * new_name: the name (ID_chrom_start-end(strand)) of the gene region
         defined here
-        new_ID: the IDs of the instances of this gene in this region
-        new_start: the minimum start position of all instances of this
+        * new_ID: the IDs of the instances of this gene in this region
+        * new_start: the minimum start position of all instances of this
         gene in this region
-        new_end: the maximum end position of all instances of this gene in
+        * new_end: the maximum end position of all instances of this gene in
         this region
-        new_strand: the strand or strands of this gene in this region
-        new_match: the reference sequences matched to these instances of
+        * new_strand: the strand or strands of this gene in this region
+        * new_match: the reference sequences matched to these instances of
         this gene using Exonerate
-        new_group: the group these instances of this gene were assigned to
-        new_genus: the genera these instance of this gene were assigned to
+        * new_group: the group these instances of this gene were assigned to
+        * new_genus: the genera these instances of this gene were assigned to
+
+    This is carried out using the the concatenated "name" column from the
+    bedtools merged output - the names of the merged regions delimited by ",".
     '''
+    # split on the delimiter
     R = region.split(",")
+    # is this gene there at all?
     this_gene = []
     for seg in R:
         if gene in seg:
             this_gene.append(seg)
+    # if this gene is present in this region
     if len(this_gene) != 0:
+        # store the IDs, starts, ends, strands
         IDs, starts, ends, strands = [], [], [], []
+        # dictionary to store the group information for this gene
         D = {c: [] for c in merge_cols}
         for ID in this_gene:
+            # get the ORF ID, chromosome, start, end and strand
             namD = ORFs.splitNam(ID)
             starts.append(namD['start'])
             ends.append(namD['end'])
@@ -92,19 +126,30 @@ def processRegion(region, gene, merge_cols, geneD):
             strands.append(namD['strand'])
             for col in merge_cols:
                 D[col].append(geneD[gene][col][namD['ID']])
+        # find the minimum start positoin
         new_start = min(starts)
+        # find the maximum end position
         new_end = max(ends)
+        # concatenate the IDs with | as a delimiter
         new_ID = "|".join(IDs)
+        # concatenate the strands (unique only)
         new_strand = "".join(uniqueList(strands))
 
+        # Make a new ID based on this info
         new_name = "%s_%s-%s-%s(%s)" % (new_ID, namD['chrom'],
                                         new_start, new_end,
                                         new_strand)
+
+        # combine this to make part of the table row
         new_row = [new_name, new_ID, new_start, new_end, new_strand]
 
+        # add the group information to the row
         for col in merge_cols:
             new_row.append("|".join(uniqueList(D[col])))
     else:
+        # if this gene isn't found in this region, use NA as a placeholder
+        # so the final output table has the same columns in every row.
+        # float('nan') is used for numeric columns
         new_row = ["NA", "NA", float('nan'), float('nan'),
                    "NA", "NA", "NA", "NA"]
     return (new_row)
@@ -134,10 +179,18 @@ def fixCol(genes, results, col):
     return (new_G)
 
 
-def cleanTable(results, bed, final_cols, keep_cols, genes):
+def cleanTable(results, bed, keep_cols, genes):
     '''
-    Tidy up the final output table - sort, merge strand and genus columns
+    Tidy up the final output table:
+        * Merge with the original bed file to get the start and end
+          co-ordinates of the region
+        * Sort by chromosome then start position
+        * Merge strand and genus columns to make a combined strand and genus
+          for all genes
+        * Find the start and end positions of the genes relative to the
+          start of the region (rather than the chromosome positions)
     for all genes, remove ununsed columns.
+
     '''
     results = bed.merge(results)
     results = results.sort_values(['name', 'chrom', 'start'])
@@ -153,28 +206,91 @@ def cleanTable(results, bed, final_cols, keep_cols, genes):
     return (results)
 
 
-def getRegions(infile, genes):
-    bed_cols, merge_cols, all_cols, final_cols, keep_cols = makeColumns(genes)
-    geneD = getDicts(genes, merge_cols)
-    bed = pd.read_csv(infile, sep="\t", header=None)
-    bed.columns = bed_cols
-    rows = []
-    regionD = dict()
+def getRegions(infile, genes, log):
+    '''
+    Takes a merged bed file consisting of regions of the genome identified
+    as having more than one ERV-like ORF, finds the regions within this file
+    which contain more than one different gene (e.g. gag and pol instead of
+    two gag ORFs) and outputs a formatted table of information about these
+    regions.
 
+    The output table will usually have 37 columns:
+        * name - the final ID of the ERV region - the genes found plus an
+          integer
+          e.g. gag_pol_12
+        * chrom - chromosome
+        * start - start position of the ERV region
+        * end - end position of the ERV region
+        * strand - strand of the ERv region
+        * genus - genus of the ERV region, can be multiple genera delimted by
+          | if different genes had different genera
+        * for each gene screened for (usually gag, pol and env)
+              * GENE_name - the names of the ORFs for this gene in this region
+              * GENE_ID - the original IDs of the ORFs for this gene in this
+                region
+              * GENE_start - the start position of this gene in this region
+                (genome co-ordinates)
+              * GENE_relative_start - the start position of this gene in this
+                region (relative to the start of the region)
+              * GENE_end - the end position of this gene in this region
+                (genome co-ordinates)
+              * GENE_relative_end - the end position of this gene in this
+                region (relative to the end of the region)
+              * GENE_strand - the strand for this gene in this region
+              * GENE_match - the closest reference retrovirus to this gene
+                in this region
+              * GENE_group - the group of the closest reference retrovirus to
+                this gene in this region
+              * GENE_genus - the genus of the closest reference retrovirus to
+                this gene in this region
+        * orig_name - the name of the region in the input table
+    '''
+    log.info("Retrieving column names for %s" % infile)
+    # get the column names for all the tables
+    bed_cols, merge_cols, all_cols, keep_cols = makeColumns(genes)
+
+    # dictionaries containing the most similar known retrovirus, group and
+    # genus of the ERV regions in the input table
+    geneD = getDicts(genes, merge_cols)
+
+    # read the bed file into a table
+    bed = pd.read_csv(infile, sep="\t", header=None)
+    # add column names
+    bed.columns = bed_cols
+
+    # empty variable to store the output table
+    rows = []
+
+    # this is used to keep count of how many regions of each type have been
+    # found so they can be named
+    regionD = dict()
+    log.info("Annotating multi-gene regions in %s" % infile)
     for region in bed['orig_name']:
+        # find out which genes are in the region based on the names of the
+        # ORFs in the region
         region_genes = checkRegion(genes, region)
         # if there is more than one different gene in the region
         if len(region_genes) > 1:
+            # the regions are named with the concatenated names of the
+            # genes which are present as a prefix e.g. gag_pol
             region_gene_ID = "_".join(region_genes)
             regionD.setdefault(region_gene_ID, 0)
+            # keep count of the number of regions with the same prefix
             regionD[region_gene_ID] += 1
+            # assign an ID - the prefix plus the next number
             region_ID = "%s_%i" % (region_gene_ID, regionD[region_gene_ID])
+            # the first column in the output should be the region ID
             row = [region_ID]
             for gene in genes:
+                # Get the details of this gene in this region to add to the
+                # row about this region
                 gene_results = processRegion(region, gene, merge_cols, geneD)
                 row += gene_results
             row.append(region)
             rows.append(row)
+    # make a data frame of all the rows
     results = pd.DataFrame(rows, columns=all_cols)
-    clean_results = cleanTable(results, bed, final_cols, keep_cols, genes)
+    log.info("Identified %i multi-gene regions in %s" % (len(results), infile))
+    # tidy up the data frame
+    clean_results = cleanTable(results, bed, keep_cols, genes)
     return (clean_results)
